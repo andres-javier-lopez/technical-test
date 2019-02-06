6564167d1b45bd726c9bf9a10b0fa988af17fe28
Andrés Javier López <ajavier.lopez@gmail.com>
2019-02-06 02:27:48 -0600

    Add frontend with product list

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,98 +1,58 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Laravel</title>
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Products</div>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <div class="card-body">
+                    <div class="form-group row">
+                        <div class="col-sm-10">
+                            <input type="text" id="search-input" class="form-control"/>
+                        </div>
+                        <div class="col-sm-2">
+                            <button id="search-btn" class="btn btn-main">Search</button>
+                        </div>
+                    </div>
+                    <ul id="product-list" class="list-group">
+                    </ul>
                 </div>
             </div>
         </div>
-    </body>
-</html>
+    </div>
+</div>
+@endsection
+
+@push('scripts')
+<script type="text/javascript" src="{{ asset('js/jquery.js') }}"></script>
+<script type="text/javascript">
+(function(){
+    "use strict";
+
+    var sortData = function(data){
+        var $list = $('#product-list');
+        $list.html('');
+        $.each(data.data, function(key, value){
+            var product = '<li class="list-group-item">';
+            product += '<h3>' + value.data.name + ' - $' + value.data.price + '</h3>';
+            product += '<p>' + value.data.description + '</p>';
+            product += '<small>' + value.data.likes + ' likes</small>';
+            product += '</li>';
+            $list.append(product);
+        });
+    };
+
+    $(document).ready(function(){
+        $("#search-input").on('change', function(){
+            var search = $('#search-input').val();
+            $.getJSON('/products?search=' + search, sortData);
+        });
+
+        $.getJSON('/products', sortData);
+    });
+
+})();
+</script>
+@endpush
